4881186e9417ebbf4cbf82b298578f3989bd0b33
Keola Dunn <keola.a.dunn@vanderbilt.edu>
2018-11-13 11:54:05 -0600

    The skeleton of the windows app

---
+++ Clippy/Clippy/AppHomeScreen.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Clippy
+{
+    /// <summary>
+    /// Interaction logic for AppHomeScreen.xaml
+    /// </summary>
+    public partial class AppHomeScreen : Window
+    {
+        public AppHomeScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+
+            //Do the things to disconnect the app from the account, save state, and return to the login window
+            MainWindow home = new MainWindow();
+            home.Show();
+            this.Close();
+
+
+        }
+
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("THIS IS MEANT TO COPY FROM CLIPBOARD");
+        }
+
+        private void Paste_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("THIS IS MEANT TO PASTE TO THE CLIPBOARD");
+        }
+    }
+}
---
+++ Clippy/Clippy/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clippy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            //This is the handling of logging in
+            String username = Username.Text;
+            String password = Password.Password;
+
+            //Log in if username and password are valid
+
+            //Switch to user's AppHome if valid login
+
+            String temp = username + "   " + password;
+
+            AppHomeScreen home = new AppHomeScreen();
+            home.Show();
+            this.Close();
+
+
+
+        }
+
+        private void connectToUserAcct(String username, String password)
+        {
+
+        }
+    }
+}
+}
